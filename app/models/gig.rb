6bc85f57da518e3292040e044c954c16d549bc73
rakib <rakib063049@gmail.com>
2013-04-22 00:48:13 +0600

    create edit update gigs and dependents

--- app/models/gig.rb
+++ app/models/gig.rb
@@ -1,7 +1,25 @@
 class Gig < ActiveRecord::Base
   attr_accessible :created_by, :details, :duration, :email, :gig_type, :name, :price, :starts_at, :venue_id, :website_url,
-      :others, :latitude, :longitude, :gmaps
+                  :others, :latitude, :longitude, :gmaps, :extra_info, :artist_id
+  attr_accessor :attr_venue, :artist_id
+
   has_many :gig_artists
   has_many :artists, through: :gig_artists
   belongs_to :venue
+
+  after_create :create_gigs_artist
+  before_save :create_gig_venue
+
+  private
+
+  def create_gig_venue
+    logger.info ">>>>> Creating venue"
+    self.create_venue!(self.attr_venue) if self.attr_venue.present?
+  end
+
+  def create_gigs_artist
+    logger.info ">>>>> Creating gigs artist"
+    GigArtist.create!(gig_id: self.id, artist_id: self.artist_id) if self.artist_id.present?
+  end
+
 end
