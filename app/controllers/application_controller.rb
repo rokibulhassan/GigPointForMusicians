bf538292e4d7f34faf2c11405d80312eee9abb4b
Irfan Ahmed <irfan@e-solutionpark.com>
2013-05-27 02:51:02 +0600

    fixing broken facebook integration system

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-        user_path(current_user)
+        user_path(resource.id)
   end
 
   def after_sign_up_path_for(resource)
