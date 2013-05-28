7dcd3de0545625c17a781d01dc3a38e4c223fb6d
rakib hasan <rakib063049@gmail.com>
2013-05-28 22:35:42 +0600

    Create venue from active admin

---
+++ app/admin/venues.rb
@@ -0,0 +1,40 @@
+ActiveAdmin.register Venue do
+  filter :name
+  filter :latitude
+  filter :longitude
+
+  index do
+    column :name, :sortable => 'venue.name' do |resource|
+      link_to resource.name, resource_path(resource)
+    end
+    column :latitude
+    column :longitude
+    column :about
+    column "Action" do |resource|
+      links = ''.html_safe
+      links += link_to "Edit", edit_resource_path(resource), :class => "member_link edit_link"
+      links += link_to "Destroy", resource_path(resource), :method => :delete, :confirm => "Are you Sure?", :class => "member_link delete_link"
+      links
+    end
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :latitude
+      f.input :longitude
+      f.input :about
+    end
+
+    f.actions
+  end
+
+  show do |venue|
+    attributes_table do
+      row :name
+      row :latitude
+      row :longitude
+      row :about
+    end
+  end
+end
