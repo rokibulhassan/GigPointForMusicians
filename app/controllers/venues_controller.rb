5fa6177232e4a022d05cecf44eb28ac533788b34
rakib hasan <rakib063049@gmail.com>
2013-05-30 13:48:02 +0600

    Get venues as json

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,5 +1,24 @@
 class VenuesController < ApplicationController
 
+
+  def index
+    venues = Venue.all
+
+    result = venues.collect do |venue|
+      {id: venue.id,
+       value: venue.name,
+       latitude: venue.latitude,
+       longitude: venue.longitude,
+       street: venue.try(:address).try(:street),
+       city: venue.try(:address).try(:city),
+       state: venue.try(:address).try(:state),
+       country: venue.try(:address).try(:country),
+       zip: venue.try(:address).try(:zip)
+      }
+    end
+    render json: result
+  end
+
   def auto_complete
     venues = Venue.auto_complete_results(params)
 
